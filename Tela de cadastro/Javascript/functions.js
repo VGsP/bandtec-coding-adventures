e2e7d1c44e81e89c7beaca614277fdb4db9d6e48
Victor Gabriel <victorgabriel213@gmail.com>
2019-05-26 19:17:39 -0300

    formularios atualizados

--- Tela de cadastro/Javascript/functions.js
+++ Tela de cadastro/Javascript/functions.js
@@ -8,6 +8,7 @@ function cadastrar() {
     semsenha.innerHTML = '';
     semconfirmarsenha.innerHTML = '';
     semcpf.innerHTML = '';
+    semidade.innerHTML = '';
 
     user.push(email.value);
 
@@ -22,7 +23,8 @@ function cadastrar() {
 
     // RESTRIÇÕES DO CADASTRO
     if (nome.value == '' || email.value == '' || senha.value == '' || confirmarsenha.value == '' ||
-        (senha.value).length < 6 || cpf.value == '' || (cpf.value).length < 11 || senha.value != confirmarsenha.value) {
+        (senha.value).length < 6 || cpf.value == '' || (cpf.value).length < 11 || idade.value == '' ||
+        (idade.value).lenght < 10 || senha.value != confirmarsenha.value) {
 
         if (nome.value == '') {
             semnome.innerHTML = '*NOME OBRIGATORIO';
@@ -39,12 +41,19 @@ function cadastrar() {
         if (confirmarsenha.value == '') {
             semconfirmarsenha.innerHTML = '*SENHA DEVE SER CONFIRMADA';
         }
+        if (idade.value == '') {
+            semidade.innerHTML = '*DATA DE NASCIMENTO<br>OBRIGATORIA';
+        }
+        else if ((idade.value).length < 10) {
+            semidade.innerHTML = '*PREENCHA O CAMPO DE <br>DATA DE NASCIMENTO';
+        }
         if (cpf.value == '') {
             semcpf.innerHTML = '*CPF OBRIGATORIO';
         }
         else if ((cpf.value).length < 11) {
             semcpf.innerHTML = '*CPF INVÁLIDO';
         }
+
         if (senha.value != confirmarsenha.value) {
             alert('É NECESSARIO CONFIRMAR SENHA CORRETAMENTE')
         }
